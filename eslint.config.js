308fb082018ce332a1ffa383cfded1766fdad43a
ManuelRoperoM <mnlrpr@gmail.com>
2024-08-19 15:12:10 -0500

    Config Standard

--- eslint.config.js
+++ eslint.config.js
@@ -2,27 +2,41 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import tsEslintParser from '@typescript-eslint/parser'
 
-export default tseslint.config(
+export default [
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      sourceType: 'module',
+      parser: tsEslintParser,
+      globals: {
+        ...globals.browser
+      }
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
+      'indent': ['error', 2],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'never'],
+      'no-unused-vars': ['error', { 'args': 'none' }],
+      'no-magic-numbers': ['error', { 'ignore': [0, 1] }],
+      'space-before-function-paren': ['error', 'never'],
+      'no-var': 'error',
+      'arrow-parens': ['error', 'always'],
+      'eol-last': ['error', 'always'],
+      'object-curly-spacing': ['error', 'never'],
+      'array-bracket-spacing': ['error', 'never'],
+      'template-curly-spacing': ['error', 'never'],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
     },
   },
-)
+]
